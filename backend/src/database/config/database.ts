590b8429b2da6e66babfcef6a646da2b599f5b5b
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-31 13:11:18 -0300

    feat: adiciona configurações para o acesso ao banco de dados pelo sequelize.

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -3,10 +3,10 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
-  database: 'cashforce_db',
+  password: process.env.DB_PASS || 'docker',
+  database: 'cashforce',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3001,
+  port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
