62b809aee479c03bbfbf448ec98611dc9113ebcf
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-31 14:49:30 -0300

    feat: implementa o componente 'Sidebar'.

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="nav">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Sidebar",
+  }
+</script>
+
+<style scoped>
+  #nav {
+    background-color: burlywood;
+    background-color: #fbfdfe;
+    display: flex;
+    align-items: center;
+    width: 288px ;
+    height: 100vh;
+  }
+
+</style>
