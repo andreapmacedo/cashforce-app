ea7c1a740d77455d9a7f6fe791a0427db4533607
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-31 13:17:48 -0300

    refactory: exclui código comentado.

--- backend/src/routes/order.route.ts
+++ backend/src/routes/order.route.ts
@@ -4,10 +4,6 @@ import OrderController from '../controllers/OrderController';
 const orderController = new OrderController();
 const route = Router();
 
-// route.get('/order', (_req,  res) => {
-//   return res.json({ message: 'teste rota' });
-// });
-
 route.get('/orders', (_req,  res, next) => orderController.findAll(_req, res, next));
 
 route.get('/order/:id', (_req,  res, next) => orderController.findOne(_req, res, next));
