09d9c9e916d40b2f5893db88855cea4bad824276
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-31 20:19:14 -0300

    feat: remove os arquivos do diretório 'dist'.

--- backend/dist/interfaces/CustomError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CustomError extends Error {
-    constructor(status, message) {
-        super(message);
-        this.status = status;
-    }
-}
-exports.default = CustomError;
