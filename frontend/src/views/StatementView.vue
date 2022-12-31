80a542c38ccf6ba8e3699ed9fd18fbed89b4a223
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-31 20:44:21 -0300

    feat: implementa a tabela com dados da requisição do banco de dados.

--- frontend/src/views/StatementView.vue
+++ frontend/src/views/StatementView.vue
@@ -5,8 +5,29 @@
       <h1>Notas fiscais</h1>
       <h4>Visualize as notas fiscais que você tem.</h4>
     </div>
-    <div class="table">
-
+    <div class="table-container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">NOTA FISCAL</th>
+            <th scope="col">SACADO</th>
+            <th scope="col">CEDENTE</th>
+            <th scope="col">EMISSÃO</th>
+            <th scope="col">VALOR</th>
+            <th scope="col">STATUS</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in data" :key="item.nNf">
+            <td scope="row">{{ item.nNf }}</td>
+            <td scope="row">{{ item.buyer.name }}</td>
+            <td scope="row">{{ item.provider.name }}</td>
+            <td scope="row">{{ item.createdAt }}</td>
+            <td scope="row">{{ item.value }}</td>
+            <td scope="row">{{ item.orderStatusBuyer }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
   </div>
@@ -19,15 +40,15 @@ export default {
   name: 'Statement',
   data() {
     return {
-      id: null,
-      orderNumver: null,
+      data: []
     };
   },
   methods: {
     async getStatement() {
-      const response = await fetch('http://localhost:3001/orders');
-      const data = await response.json();
+      const req = await fetch('http://localhost:3001/orders');
+      const data = await req.json();
       console.log(data);
+      this.data = data;
     }
   },
   mounted() {
