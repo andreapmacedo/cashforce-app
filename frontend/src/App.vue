360263fb8718c75f95b725019ca8a1002bcc2ad2
andreapmacedo <aa.andremacedo@gmail.com>
2022-12-31 13:32:50 -0300

    feat: implementa construção da base do front-end com o vite.

---
+++ frontend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <router-view />    
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: function() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "cachforce"
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
