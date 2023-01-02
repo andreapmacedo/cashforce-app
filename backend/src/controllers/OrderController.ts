f81c38fa67e0104b3a4de86417def3f677241523
andreapmacedo <aa.andremacedo@gmail.com>
2023-01-02 20:54:02 -0300

    config: adiciona configuração para o sequelize.

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -12,14 +12,4 @@ export default class TeamController {
       next(error);
     }
   };
-
-  findOne: RequestHandler = async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const result = await this.service.findOne(id);
-      return res.status(200).json(result);
-    } catch (error) {
-      next(error);
-    }
-  };
 }
--- backend/src/routes/order.route.ts
+++ backend/src/routes/order.route.ts
@@ -6,6 +6,4 @@ const route = Router();
 
 route.get('/orders', (_req,  res, next) => orderController.findAll(_req, res, next));
 
-route.get('/order/:id', (_req,  res, next) => orderController.findOne(_req, res, next));
-
 export default route;
