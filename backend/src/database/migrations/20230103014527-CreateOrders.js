7abfe08ac0a67aad56cbe3b5118683ea087d6a82
andreapmacedo <aa.andremacedo@gmail.com>
2023-01-02 22:54:41 -0300

    feat: adiciona migrations.

---
+++ backend/src/database/migrations/20230103014527-CreateOrders.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nNf: {
+        type: Sequelize.STRING
+      },
+      value: {
+        type: Sequelize.STRING
+      },
+      buyerID: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'buyers',
+          key: 'id',
+        },
+      },
+      providerID: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'buyers',
+          key: 'id',
+        },
+      },
+      orderStatusBuyer: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('orders');
+  }
+};
